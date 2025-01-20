fb37fded00455936f826653b455c6a0737b8986f
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2025-01-20 23:18:55 +0530

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,50 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'w-306-mealy',
+        short_name: 'mealy',
+        description: 'Tracking food in abode',
+        theme_color: '#121212',
+        background_color: '#121212',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        exclude: [/firebase-messaging-sw\.js$/],
+      }
+    })
+  ],
   server: {
     headers: {
       'Service-Worker-Allowed': '/'
     }
   },
   build: {
-    // Copy firebase-messaging-sw.js to build output
     rollupOptions: {
       input: {
         app: './index.html',
