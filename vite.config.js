1cbb873980cf340ad50523f3298339068dde8730
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2025-01-20 23:20:54 +0530

    pwa fixed

--- vite.config.js
+++ vite.config.js
@@ -35,8 +35,27 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        exclude: [/firebase-messaging-sw\.js$/],
-      }
+        navigateFallback: 'index.html',
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      },
+      strategies: 'injectManifest',
+      filename: 'sw.js',
+      manifestFilename: 'manifest.webmanifest'
     })
   ],
   server: {
